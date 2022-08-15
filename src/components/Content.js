7ee5756b7432fd3e8c7a8ebae18c6dc80debbef9
Astrovik3 <juantomaz13@gmail.com>
2022-08-14 22:02:10 -0300

    modifs in presentation

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,6 +8,18 @@ const Content = () => {
 
   return(
     <Grid className="second">
+      <Grid className="secondSub">
+        <Grid className="secondItem1">
+          
+        </Grid>
+        <Grid className="secondItem2">
+
+        </Grid>
+        <Grid className="secondItem3">
+
+        </Grid>
+      </Grid>
+
       <p className="secondText"> TESTING TESTING TESTING </p>
 
     </Grid>
--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -9,7 +9,7 @@ const Presentation = () => {
   return(
     <Grid className="first">
       
-      <Grid className="firstSub">
+      <Grid className="firstSub1">
         <Grid className="firstPhoto">
 
         </Grid>
@@ -18,6 +18,23 @@ const Presentation = () => {
           <p className="firstSubtext">Frontend Developer</p>
         </Grid>
 
+      </Grid>
+      <Grid className="firstSub2">
+        <p className="firstTextMenu"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  
+          <text className="firstAbout"> about</text> mauris cursus mattis molestie a iaculis at. Urna neque viverra justo nec ultrices. Ultrices dui sapien eget mi. 
+          Odio euismod lacinia at quis risus sed vulputate odio ut. Quam elementum pulvinar etiam non. 
+          Pulvinar pellentesque habitant morbi tristique senectus. Viverra mauris in aliquam sem fringilla ut morbi tincidunt augue. 
+          Leo in vitae turpis massa <text className="firstAbout"> technologies</text> sed tempus. Nisl nisi scelerisque eu ultrices. Risus at ultrices mi tempus imperdiet. 
+          Orci eu lobortis elementum nibh tellus molestie nunc. Eleifend quam adipiscing vitae proin sagittis <text className="firstAbout"> projects</text> rhoncus. 
+          Mattis vulputate enim nulla aliquet. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum integer. 
+          In nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Ac turpis egestas integer eget aliquet nibh praesent. 
+          Tincidunt ornare massa eget egestas purus viverra accumsan in nisl. Senectus et netus et malesuada fames ac. 
+          Molestie at elementum eu facilisis sed. Ullamcorper a lacus vestibulum sed arcu non odio euismod. Felis donec et odio pellentesque diam. 
+          Et <text className="firstAbout"> contact</text> ac feugiat sed lectus vestibulum mattis. Augue lacus viverra vitae congue eu consequat. 
+          Erat velit scelerisque in dictum non consectetur a. Id ornare arcu odio ut sem nulla. Accumsan sit amet nulla facilisi. 
+          Habitasse platea dictumst vestibulum rhoncus est pellentesque elit. At risus viverra adipiscing at in tellus integer. 
+          Feugiat sed lectus vestibulum mattis ullamcorper. Adipiscing elit pellentesque habitant morbi tristique. 
+        </p>
       </Grid>
 
     </Grid>
